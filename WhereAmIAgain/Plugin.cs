f683b9053accfa2dedbfafe1ea38cae0face1f4c
cassie <gh@cassie.fish>
2022-01-13 01:27:16 -0700

    filter ], should really just switch to regex when i learn c#

--- WhereAmIAgain/Plugin.cs
+++ WhereAmIAgain/Plugin.cs
@@ -24,7 +24,7 @@ namespace WhereAmIAgain
         internal static ClientState ClientState { get; private set; }
         [PluginService]
         internal static Framework Framework { get; private set; }
-        public readonly string[] notZones = { "retainer", "equipped", "sale", "guildhest", "regist", "markets", "compass", "current", "sampling", "playing", "orchestrion", ".", "action ", "!" };
+        public readonly string[] notZones = { "retainer", "equipped", "sale", "guildhest", "regist", "markets", "compass", "current", "sampling", "playing", "orchestrion", ".", "action ", "!", "]" };
         private List<TerritoryType> Territories;
         public string playerZone = "";
         private void UpdateLocation(Framework framework)
