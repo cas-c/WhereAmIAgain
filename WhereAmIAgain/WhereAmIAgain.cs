a11cc3434c76ee3b6ac530866f5827583446b2a8
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-10-02 19:17:59 -0700

    Version 1.0.0.0

--- WhereAmIAgain/WhereAmIAgain.cs
+++ WhereAmIAgain/WhereAmIAgain.cs
@@ -1,16 +1,12 @@
-﻿using Dalamud.Interface.GameFonts;
-using Dalamud.Plugin;
+﻿using Dalamud.Plugin;
 
 namespace WhereAmIAgain;
 
 public sealed class WhereAmIAgainPlugin : IDalamudPlugin
 {
-    public string Name => "Where am I again?";
-
     public static Configuration Configuration { get; set; } = null!;
     public static ConfigurationWindow ConfigurationWindow { get; set; } = null!;
     public static DtrDisplay DtrDisplay { get; set; } = null!;
-    public static GameFontHandle AxisFont { get; set; } = null!;
     
     public WhereAmIAgainPlugin(DalamudPluginInterface pluginInterface)
     {
@@ -18,14 +14,12 @@ public sealed class WhereAmIAgainPlugin : IDalamudPlugin
             
         Configuration = pluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-        AxisFont = Service.PluginInterface.UiBuilder.GetGameFontHandle(new GameFontStyle(GameFontFamilyAndSize.Axis12));
         DtrDisplay = new DtrDisplay();
         ConfigurationWindow = new ConfigurationWindow();
     }
 
     public void Dispose()
     {
-        AxisFont.Dispose();
         ConfigurationWindow.Dispose();
         DtrDisplay.Dispose();
     }
